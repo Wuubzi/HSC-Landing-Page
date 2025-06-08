a9d0ebe4267afbd470003a98138eeb06d229dd3d
wuubzi <carlosasalas321@gmail.com>
2025-06-08 00:06:26 -0500

    add a header desktop and fixed any bugs with mobile design, and bg hero

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,12 +1,33 @@
 const burguer = document.querySelector(".header-hamburguer-container");
 const close = document.querySelector(".header-close-container");
 const nav = document.querySelector(".header-nav");
+const trigger = document.querySelector('.language-trigger');
+const dropdown = document.getElementById('language-dropdown');
+const arrow = document.getElementById('dropdown-arrow');
+let isDropdownOpen = false;
+
 
 burguer.addEventListener("click", () => {
  nav.classList.toggle("active");
+ document.body.classList.toggle("no-scroll");
 });
 
 close.addEventListener("click", () => {
  nav.classList.remove("active");
 });
+
+
+  trigger.addEventListener('click', () => {
+    isDropdownOpen = !isDropdownOpen;
+
+    if (isDropdownOpen) {
+      dropdown.classList.add('open');
+      arrow.classList.add('open');
+      trigger.classList.add('active');
+    } else {
+      dropdown.classList.remove('open');
+      arrow.classList.remove('open');
+      trigger.classList.remove('active');
+    }
+  });
 
